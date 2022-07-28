4049e237a96e5407eeee0a5ccf13d0d2da180e7a
dhirajdalvi2001 <dhirajdalvi2001@gmail.com>
2022-07-28 21:05:07 +0530

    getting ready for REDUX

--- FrontEnd/src/index.js
+++ FrontEnd/src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Route, Router, Routes } from 'react-router-dom';
+import { configureStore  } from 'redux';
 import App from './App';
 import Body from './components/Body';
 import Login from './components/Login';
 import My404Component from './components/My404Component';
 import SignUp from './components/SignUp';
 import reportWebVitals from './reportWebVitals';
+
+// STORE - GLOBALIZED STATE 
+// ACTION 
+// REDUCER 
+// DISPATCH 
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
