4304a8d87231db0a823712b61eb45dde1d97958a
dhirajdalvi2001 <dhirajdalvi2001@gmail.com>
2022-08-03 17:55:47 +0530

    chat box started

--- FrontEnd/src/components/ChatBox.jsx
+++ FrontEnd/src/components/ChatBox.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { ChatMessageDto } from '../model/ChatMessageDto';
 
 function ChatBox() {
+  const [chatMessages, setChatMessages] = useState([
+    new ChatMessageDto('100RBH', 'Hi')
+  ]);
+  const [message, setMessage] = useState("");
+
+  const listOfMessages = chatMessages.map((chatMessage, index) => {
+    return (
+      <div key={index}>
+        <p>{`${chatMessage.user} : ${chatMessage.message} `}</p>
+      </div>
+    );
+  })
   return (
     <div className='chat-box'>
-      <p>ChatBox</p>
+      {listOfMessages}
+      <div className='chat-box_bottom'>
+        <div className='chat-box_bottom-user'>
+        
+        </div>
+        <div className='chat-box_bottom-message'>
+          
+        </div>
+      </div>
     </div>
   )
 }
 
-export default ChatBox
+export default ChatBox;
--- FrontEnd/src/components/RouteS.jsx
+++ FrontEnd/src/components/RouteS.jsx
@@ -4,13 +4,13 @@ import Body from './Body';
 import Login from './Login';
 import SignUp from './SignUp';
 import My404Component from './My404Component';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 function RouteS() {
   const isLoggedIn = useSelector(state => state.isLoggedIn);
   return (
         <Routes>
-          <Route exact path="/" element={isLoggedIn ? <Body /> : <Login />} />
+          <Route exact path="/" element={isLoggedIn ? <Body /> : <Navigate to="/login" />} />
           <Route exact path="/login" element={isLoggedIn ? <Navigate to="/" /> : <Login />} />
           <Route exact path="/sign-up" element={isLoggedIn ? <Navigate to="/" /> : <SignUp />} />
           <Route exact path="*" element={<My404Component />} />
